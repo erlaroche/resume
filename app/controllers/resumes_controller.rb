7dcf7c322aaca26dced658090337b89b6a86b9b3
erlaroche <E.R.Laroche@gmail.com>
2014-01-30 12:20:56 -0600

    Refactor code in view to erb

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,8 +1,15 @@
 class ResumesController < ApplicationController
 
 def show_resume
-  @name = "Emma Laroche"
-  @email = "E.R.Laroche@gmail.com"
+  @resume = {
+  name: "Emma Laroche",
+  email: "E.R.Laroche@gmail.com",
+  tech: "TECHNOLOGY",
+  server: "Server-side: Ruby on Rails, Active Record, Data Modeling, Controller logic, ERB & HAML templating, User Authentication with RSPEC",
+  client: "Client-side: Javascript, jQuery, Bootstrap, Foundation, SASS, CSS3, HTML5, AJAX, AND JSON",
+  skills: "SKILLS SUMMARY"
+}
+
 end
 
 end
