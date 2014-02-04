1239504511fd7401bd0498a1fb359f3d5a00898d
erlaroche <E.R.Laroche@gmail.com>
2014-02-03 22:58:44 -0600

    Attempt parallax scroll using skrollor plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,15 @@
 //= require turbolinks
 //= require_tree .
 
-$('parallax').stellar();
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
 
-// $(function(){
-//   $.stellar({
-//     horizontalScrolling: false,
-//     verticalOffset: 150
-//   });
-// });
+
+
